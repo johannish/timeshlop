04a0b251905d483dc4a1d1e61cdf1505f9258c20
John <raztus@gmail.com>
2013-09-27 10:34:32 -0600

    Initial simplification: remove many-many relationships
    Currently I'd rather deal with the duplicate data than manage that
    extra complexity.  Locations and people will be searchable, but will be
    duplicated for each event.

--- time-sqlite.tcl
+++ time-sqlite.tcl
@@ -33,24 +33,41 @@ proc deleteEvent {date locations people note attachments} {
 
 proc initDb {} {
 	puts "Creating DB tables"
-	db eval { CREATE TABLE IF NOT EXISTS event(id INTEGER PRIMARY KEY, crt_date INTEGER, name TEXT, date INTEGER, note TEXT) }
-	db eval { CREATE TABLE IF NOT EXISTS location(id INTEGER PRIMARY KEY, crt_date INTEGER, place TEXT, lat REAL, long REAL) }
-	db eval { CREATE TABLE IF NOT EXISTS person(id INTEGER PRIMARY KEY, crt_date INTEGER, fname TEXT, lname TEXT) }
-	db eval { CREATE TABLE IF NOT EXISTS attachment(id INTEGER PRIMARY KEY, crt_date INTEGER, file BLOB, name TEXT) }
 	db eval {
-		CREATE TABLE IF NOT EXISTS event_locations(
-			event_id INTEGER,
-			location_id INTEGER,
-			FOREIGN KEY(event_id) REFERENCES event(id),
-			FOREIGN KEY(location_id) REFERENCES location(id)
+		CREATE TABLE IF NOT EXISTS event(
+			id INTEGER PRIMARY KEY,
+			crt_date INTEGER,
+			name TEXT,
+			date INTEGER,
+			note TEXT
 		)
 	}
 	db eval {
-		CREATE TABLE IF NOT EXISTS event_persons(
+		CREATE TABLE IF NOT EXISTS location(
+			id INTEGER PRIMARY KEY,
+			crt_date INTEGER,
 			event_id INTEGER,
-			person_id INTEGER,
-			FOREIGN KEY(event_id) REFERENCES event(id),
-			FOREIGN KEY(person_id) REFERENCES person(id)
+			name TEXT,
+			lat REAL,
+			long REAL,
+			FOREIGN KEY (event_id) REFERENCES event(id)
+		)
+	}
+	db eval {
+		CREATE TABLE IF NOT EXISTS person(
+			id INTEGER PRIMARY KEY,
+			crt_date INTEGER,
+			event_id INTEGER,
+			name TEXT,
+			FOREIGN KEY (event_id) REFERENCES event(id)
+		)
+	}
+	db eval {
+		CREATE TABLE IF NOT EXISTS attachment(
+			id INTEGER PRIMARY KEY,
+			crt_date INTEGER,
+			file BLOB,
+			name TEXT
 		)
 	}
 	db eval {
@@ -64,7 +81,7 @@ proc initDb {} {
 }
 
 proc ensureSchema {} {
-	set expectedCount 7
+	set expectedCount 5
 	set tableCount [db eval {
 		SELECT count(*)
 		FROM sqlite_master
