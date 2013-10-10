04b75b9261bda9acdbdfa66d57244c676b8361a2
John <raztus@gmail.com>
2013-10-09 21:17:36 -0600

    Add filepath entry box

--- timeshlop.tcl
+++ timeshlop.tcl
@@ -11,7 +11,7 @@ set events [getAllEvents]
 #[import ./events.tcld]
 
 wm title . "Timeshlop"
-wm minsize . 200 100
+wm minsize . 400 200
 # Padding is w n e s
 grid [ttk::frame .c -padding "5 5 10 10" -relief raised] -column 0 -row 0 -sticky wnes
 # Make ttk::frame resize with window
@@ -19,11 +19,14 @@ grid columnconfigure . 0 -weight 1
 grid rowconfigure . 0 -weight 1
 
 grid [tk::listbox .c.eventlb -height 10 -listvariable events] -column 0 -row 0 -sticky wn
-grid [ttk::button .c.importbtn -text "Import file" -command importMe] -column 1 -row 0 -sticky n
-grid [ttk::label .c.messages -text "msg"] -column 0 -row 1 -columnspan 2 -sticky ws
+grid [ttk::entry .c.importentry -text "File path"] -column 1 -row 0 -sticky n
+grid [ttk::button .c.importbtn -text "Import file" -command importMe] -column 2 -row 0 -sticky n
+grid [ttk::label .c.messages] -column 0 -row 1 -columnspan 2 -sticky ws
+#grid [ttk::sizegrip .c.grip] -column 2 -row 1 -sticky se #TODO Make this non-stationary
 
 proc importMe {} {
-	set importmsg [import ./events.dat]
+	set path [.c.importentry get]
+	set importmsg [import $path]
 	.c.messages configure -text $importmsg
 	#TODO Refresh events in listbox
 }
